f09e20eb524967ba5bbefcbe2c91f737950b30f1
Derek Haller <dhaller.personal@gmail.com>
2017-10-06 03:47:54 +0000

    automatically backed up by learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -5,3 +5,13 @@ def display_board(board)
   puts "-----------"
   puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
+
+def valid_move(board, index)
+  if board[index] == ("X" || "O")
+    return false
+  elsif index > 8
+    return false
+  else
+    return true
+  end
+end
