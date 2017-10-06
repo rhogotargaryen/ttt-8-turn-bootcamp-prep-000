73bc61b9bde9dfbc782bc579505f8410c50f45de
Derek Haller <dhaller.personal@gmail.com>
2017-10-06 05:01:55 +0000

    automatically backed up by learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -18,7 +18,7 @@ end
 def valid_move?(board, index)
   if position_taken(board, index)
     return false
-  elsif !index.between?(0,8)
+  elsif index.between?(0,8)
     return false
   else
     return true
