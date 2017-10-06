960cca0981d4c46313c9032d88d826b8e2660920
Derek Haller <dhaller.personal@gmail.com>
2017-10-06 04:05:36 +0000

    automatically backed up by learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -5,12 +5,13 @@ def display_board(board)
   puts "-----------"
   puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
-
+def input_to_index (input)
+  index = input.to_i - 1
 def valid_move(board, index)
   if board[index] == ("X" || "O")
     return false
-  elsif index > 8
-    return false
+  elsif index >= 8
+    return true
   else
     return true
   end
