cb8a94245e2842578ecff126437027f673c85166
Derek Haller <dhaller.personal@gmail.com>
2017-10-06 04:21:58 +0000

    automatically backed up by learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -9,14 +9,14 @@ def input_to_index (input)
   index = input.to_i - 1
 end
 
-def position_taken (board, index)
+def position_taken(board, index)
   if board[index] == ("X" || "O")
     return true
   end
 end
 
 def valid_move?(board, index)
-  if position_taken (board, index)
+  if position_taken(board, index)
     return false
   elsif index > 8
     return false
